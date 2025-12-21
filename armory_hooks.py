90f931ed06cf538bf678b8d08133c34d7d094fe4
Moisés Peláez <moises.jpelaez@gmail.com>
2025-12-21 17:43:34 -0400

    Update armory_hooks

--- armory_hooks.py
+++ armory_hooks.py
@@ -1,7 +1,7 @@
 def write_main():
     return {
         'imports': 'import koui.Koui;',
-        'main_post': 'iron.data.Data.getBlob("ui_override.ksn", function(_) { Koui.init(function() { iron.App.notifyOnRender2D(function(g) { g.end(); Koui.render(g); g.begin(false); }); }); });',
+        'main_post': 'iron.data.Data.getBlob("ui_override.ksn", function(_) { Koui.init(function() { iron.App.notifyOnRender2D(function(g) { g.end(); @:privateAccess Koui.g.imageScaleQuality = armory.ui.Canvas.imageScaleQuality; Koui.render(g); g.begin(false); }); }); });',
         'assets': [
             'Assets/koui_canvas/ui_override.ksn'
         ]
